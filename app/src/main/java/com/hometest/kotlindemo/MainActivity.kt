9bd96a2f808154bf74dbbca5bafd63556cd01a00
admin <kunalsuthar285@gmail.com>
2022-07-06 16:13:49 +0530

    Initial commit

---
+++ app/src/main/java/com/hometest/kotlindemo/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.hometest.kotlindemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.hometest.kotlindemo.databinding.ActivityMainBinding
+
+    lateinit var binding : ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding:ActivityMainBinding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       // var str=binding.tv1.text.toString()
+        binding.btn.setOnClickListener{
+            Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
+            Log.d("name",binding.tv1.text.toString())
+        }
+    }
+}
